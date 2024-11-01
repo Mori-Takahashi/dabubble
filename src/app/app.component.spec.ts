28b92227179df7e8619b34611e28bdc3ee3fb4cb
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-11-01 14:00:31 +0100

    add Login

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'dabubble' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('dabubble');
+    expect(app.title).toEqual('DABubble');
   });
 
   it('should render title', () => {
